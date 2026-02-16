6ba2ee19d7a1b698da474bf854f2cbb07099255f
Rodrigo Moreira <rodrigomoreira.dev@gmail.com>
2026-02-16 00:01:58 -0300

    Teste resolução de bugs

---
+++ Node/02-MyContacts/src/routes.js
@@ -0,0 +1,14 @@
+const { Router } = require("express");
+const router = Router();
+const ContactController = require("./app/controllers/contactController");
+
+router.get("/", (request, response) => {
+  response.send("hello world 3");
+});
+
+router.get("/contacts", ContactController.index);
+router.get("/contacts/:id", ContactController.show);
+router.delete("/contacts/:id", ContactController.delete);
+router.post("/contacts/", ContactController.store);
+router.put("/contacts/:id", ContactController.update);
+module.exports = router;
