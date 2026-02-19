5269f339e3a7963de5b6e63d7155859e941df094
Rodrigo Moreira <rodrigomoreira.dev@gmail.com>
2026-02-18 21:03:53 -0300

    css inline

--- React/01-Fundamentos/src/Post.jsx
+++ React/01-Fundamentos/src/Post.jsx
@@ -3,7 +3,11 @@ import React from "react";
 function Post(props) {
   return (
     <article>
-      <h2>
+      <h2
+        style={{
+          fontFamily: "sans-serif",
+        }}
+      >
         {props.read ? <s>{props.title}</s> : props.title}{" "}
         <button onClick={() => props.onRemove(props.id)}>Remover</button>
       </h2>
