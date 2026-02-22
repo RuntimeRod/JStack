52f9ba0fc3a7255fb36e985ff98d5c45ba3ce031
Rodrigo Moreira <rodrigomoreira.dev@gmail.com>
2026-02-21 23:14:28 -0300

    Ajuste WebPack Config

--- React/01-Fundamentos/webpack.config.js
+++ React/01-Fundamentos/webpack.config.js
@@ -38,7 +38,15 @@ module.exports = {
         test: /\.css?/,
         exclude: /node_modules/,
         //Nessa ordem sytle-loader primeiro
-        use: ["style-loader", "css-loader" ],
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+        ],
       },
     ],
   },
